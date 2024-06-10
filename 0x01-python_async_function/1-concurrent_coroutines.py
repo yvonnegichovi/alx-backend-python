c9a3d70cb5bb8d7aeaa39b46b1d07f1fbafa9bc5
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-10 23:07:20 +0300

    Task 1
    fixing importation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,10 +6,12 @@ This module shows aysnchronous and await functionalities
 import asyncio
 from typing import List
 from heapq import nsmallest
-from 0-basic_async_syntax import wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     This function runs 'wait_random' n times concurrently
     The results are collected and returned in ascending order
