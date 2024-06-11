a3d36585a88c24f8e4b66c2902c2076d2e77ab70
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-11 23:45:21 +0300

    Task 0
    sleep and yield

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module has coroutine asynchronous functions
+"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Loops 10 times, each time asynchronously wait 1 second,
+    then yield a random number between 0 and 10
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
