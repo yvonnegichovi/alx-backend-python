7f29e603e42081d00d03948bdfefba3835e762e0
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-12 00:33:49 +0300

    Task 1
    fixing errors

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,6 +3,7 @@
 This module has coroutine asynchronous functions
 """
 
+from typing import List
 import asyncio
 import random
 
@@ -10,7 +11,7 @@ import random
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Collects 10 random numbers using async comprehensing over
     async_generator, then return 10 random numbers
