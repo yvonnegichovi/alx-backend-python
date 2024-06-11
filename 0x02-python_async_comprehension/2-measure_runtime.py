db3e9a99d2424114e4dbac4e69c52e13609d0ad8
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-12 00:25:20 +0300

    Task 2
    runtime

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module has coroutine asynchronous functions
+"""
+
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """
+    Measure the total runtime of executing async_comprehension four times in parallel.
+    """
+    start_time = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = time.perf_counter()
+    return end_time - start_time
