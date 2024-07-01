6176a8c2d14fea1d1f94375227777bd7ef43be13
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-01 16:59:42 +0300

    parameterization

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,34 @@
 This module has tests
 """
 
+from parameterized import parameterized
+import unittest
+
+
+def access_nested_map(nested_map, path):
+    """
+    This method tests parameterization
+    """
+    current = nested_map
+    for key in path:
+        current = current[key]
+    return current
+
 
 class TestAccessNestedMap(unittest.TestCase):
     """
     This class parameterize a unit test
     """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
