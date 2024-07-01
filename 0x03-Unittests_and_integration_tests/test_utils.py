7b438584609dd9aa9e12f0762899c3f6b6aaf3eb
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-01 17:09:44 +0300

    parameterization while raising KeyError

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -29,7 +29,23 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
 
     def test_access_nested_map(self, nested_map, path, expected):
+        """
+        Tests access_nested_map with parameterized inputs
+        """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a")),
+        ({"a": 1}, ("a", "b")),
+    ])
+
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Tests access_nested_map raises KeyError with parameterized inputs
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), str(KeyError(path[-1])))
 
 
 if __name__ == "__main__":
