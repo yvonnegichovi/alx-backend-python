daf78b3a8320df05f5adee725207239735898fb9
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-05 21:28:52 +0300

    task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -43,25 +43,28 @@ class TestGetJson(unittest.TestCase):
     """
     This class is a mock HTTP calls
     """
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False}),
-    ])
-
     @patch('utils.requests.get')
-    def test_get_json(self, test_url, test_payload, mock_get):
+    def test_get_json(self, mock_get):
         """
         Tests that utils.get_json returns the expected result
         """
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
-        mock_response.return_value = mock_response
-
-        result = get_json(test_url)
-
-        mock_get.assert_called_once_with(test_url)
-
-        self.assertEqual(result, test_payload)
+        test_cases = [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False})
+        ]
+        
+        for test_url, test_payload in test_cases:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+            
+            result = get_json(test_url)
+            
+            mock_get.assert_called_once_with(test_url)
+            
+            self.assertEqual(result, test_payload)
+            
+            mock_get.reset_mock()
 
 
 if __name__ == "__main__":
