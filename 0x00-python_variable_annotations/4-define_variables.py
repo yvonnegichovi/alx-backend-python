a149100ea52a2784f72d34a99dc4129fafba7d7c
yvonnegichovi <yvonnegichovi@gmail.com>
2024-08-23 19:21:54 +0300

    basic annotations

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This is how you can define and annotate the variables
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
