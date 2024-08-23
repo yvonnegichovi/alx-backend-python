a693f61bc2cf6cc0f2eb91770e66706db5df1c23
yvonnegichovi <yvonnegichovi@gmail.com>
2024-08-23 19:35:16 +0300

    basic annotations

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -7,13 +7,15 @@ from typing import Sequence, Any, Union
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
-    Returns the first element of a sequence if it is not empty; otherwise, returns None.
+    Returns the first element of a sequence if it is not empty;
+    otherwise, returns None.
 
     Args:
         lst (Sequence[Any]): A sequence (e.g., list, tuple) of any type.
 
     Returns:
-        Union[Any, None]: The first element of the sequence or None if the sequence is empty.
+        Union[Any, None]: The first element of the sequence or
+        None if the sequence is empty.
     """
     if lst:
         return lst[0]
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -8,13 +8,16 @@ from typing import Iterable, Sequence, List, Tuple
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    Returns a list of tuples where each tuple contains an element from the input iterable
+    Returns a list of tuples where each tuple contains
+    an element from the input iterable
     and the length of that element.
 
     Args:
-        lst (Iterable[Sequence]): An iterable of sequences (e.g., strings, lists, tuples).
+        lst (Iterable[Sequence]): An iterable of sequences
+        (e.g., strings, lists, tuples).
 
     Returns:
-        List[Tuple[Sequence, int]]: A list of tuples, each containing a sequence and its length.
+        List[Tuple[Sequence, int]]: A list of tuples,
+        each containing a sequence and its length.
     """
     return [(i, len(i)) for i in lst]
