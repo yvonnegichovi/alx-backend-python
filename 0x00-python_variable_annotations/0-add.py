04d3ed5eaabfd0a397c7d6098bbba80b96bdc53f
yvonnegichovi <yvonnegichovi@gmail.com>
2024-08-23 19:13:27 +0300

    basic annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,5 +3,8 @@
 This module shows type-annotated functionalities
 """
 
+
 def add(a: float, b: float) -> float:
+    """
+    takes a float a and b as arguments and returns their sum"""
     return a + b
