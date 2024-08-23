7cd51cc0b25dc63c5067f25469c9da99010f9447
yvonnegichovi <yvonnegichovi@gmail.com>
2024-08-23 19:45:27 +0300

    basic annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python3
 """
-This is an advanced task for basic annontation
+This is an advanced task for basic annotation
 """
 
 from typing import Mapping, Any, Union, TypeVar
 
 T = TypeVar('T')
 
-
-def safely_get_value(dct: Mapping[Any, T], key: Any, default: Union[T, None] = None) -> Union[T, None]:
+def safely_get_value(dct: Mapping[Any, T], key: Any, default: Union[T, None] = None) -> Union[Any, T]:
     """
     Returns the value for the given key from the dictionary if it exists;
     otherwise, returns the default value.
 
     Args:
-        dct (Mapping[Any, T]): A mapping (e.g., dictionary)
-        with keys of any type and values of type T.
+        dct (Mapping[Any, T]): A mapping (e.g., dictionary) with keys of any type and values of type T.
         key (Any): The key to look up in the dictionary.
-        default (Union[T, None]): The value to return if the key is not found;
-        defaults to None.
+        default (Union[T, None]): The value to return if the key is not found; defaults to None.
 
     Returns:
-        Union[T, None]: The value associated with the key or
-        the default value if the key is not found.
+        Union[Any, T]: The value associated with the key or the default value if the key is not found.
     """
     if key in dct:
         return dct[key]
