b4c186ce2a6c8fe11ac4b5cf09e4759b1514bffd
yvonnegichovi <yvonnegichovi@gmail.com>
2025-07-23 01:17:01 +0300

    feat: Add user authentication using JWT (JSON Web Tokens) or Session Authentication.

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'chats',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -133,5 +134,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
     ],
 }
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -22,4 +22,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('chats.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
